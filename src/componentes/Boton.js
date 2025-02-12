b7ab6415e6b891e3ffe588ed8ebe27cf5a713aa4
pablodelpino98 <pablo19delpino98@gmail.com>
2025-02-12 20:01:18 +0000

    Modificar Boton.js y añadir BotonClear.js y Pantalla.js

--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -1,8 +1,14 @@
 import React from "react";
+import "../hojas-de-estilo/Boton.css";
 
 function Boton(props) {
+
+    const esOperador = valor => {
+        return isNaN(valor) && (valor != '.') && (valor != '=');
+    }
+
     return (
-        <div>
+        <div className={`boton-contenedor${esOperador(props.children) ? ' operador' : ''}`} onClick={() => props.manejarClick(props.children)}>
             {props.children}
         </div>
     )
