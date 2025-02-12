c82cc032828941f48e79387ba23faa427a354fb9
pablodelpino98 <pablo19delpino98@gmail.com>
2025-02-12 16:07:02 +0000

    Modificación de App.js y añado carpeta componentes con archivo Boton.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Boton from './componentes/Boton.js';
 
 function App() {
   return (
-    <div className="App">
-      Hola
+    <div className='App'>
+      <h1>Calculadora Interactiva</h1>
+      <div className='contenedor-calculadora'>
+        <div className='fila'>
+        </div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+      </div>
     </div>
   );
 }
